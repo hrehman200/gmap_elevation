dd64be4b78c8c56052907c146cc6125f3e0d49e6
hrehman200 <hrehman200@gmail.com>
2017-11-27 22:52:47 +0500

    Enter miles to drive and get find out roads around your location with elevation data

---
+++ js/custom.js
@@ -0,0 +1,256 @@
+google.load('visualization', '1', {packages: ['columnchart']});
+
+function initMap() {
+
+    var elevator = new google.maps.ElevationService;
+    var infowindow = new google.maps.InfoWindow({map: map});
+    var directionsRenderer = new google.maps.DirectionsRenderer();
+    var directionsService = new google.maps.DirectionsService();
+
+    var mapOptions = {
+        zoom: 20
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var markers = [];
+
+    var currentLocation = null;
+    var circle = null;
+    var polylines = [];
+    var routes = [];
+    var elevationsData = [];
+
+    directionsRenderer.setMap(map);
+
+    // Add a listener for the click event. Display the elevation for the LatLng of
+    // the click inside the infowindow.
+    /*map.addListener('click', function (event) {
+     displayLocationElevation(event.latLng, elevator, infowindow);
+     placeMarker(event.latLng);
+     });*/
+
+    function findLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                currentLocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                var marker = new google.maps.Marker({
+                    position: currentLocation,
+                    map: map,
+                    icon: 'http://i.stack.imgur.com/orZ4x.png'
+                });
+                marker.setMap(map);
+
+                map.setCenter(currentLocation);
+                drawCircle();
+
+            }, function () {
+                alert('Failed in finding location');
+            });
+        }
+    }
+
+    function calcRoute() {
+
+        var start = new google.maps.LatLng(currentLocation.lat, currentLocation.lng);
+        var end = new google.maps.LatLng(markers[0].getPosition().lat(), markers[0].getPosition().lng());
+
+        var bounds = new google.maps.LatLngBounds();
+        bounds.extend(start);
+        bounds.extend(end);
+        //map.fitBounds(bounds);
+        var request = {
+            origin: start,
+            destination: end,
+            travelMode: google.maps.TravelMode.DRIVING,
+            provideRouteAlternatives: true
+        };
+        directionsService.route(request, function (response, status) {
+            // clear former polylines
+            for (var j in  polylines) {
+                polylines[j].setMap(null);
+            }
+            polylines = [];
+
+            if (status == google.maps.DirectionsStatus.OK) {
+                var bounds = new google.maps.LatLngBounds();
+                // draw the lines in reverse orde, so the first one is on top (z-index)
+                for (var i = response.routes.length - 1; i >= 0; i--) {
+                    // let's make the first suggestion highlighted;
+                    var line = drawPolyline(response.routes[i].overview_path, '#ffffff');
+                    polylines.push(line);
+                    routes.push(response.routes[i].overview_path);
+
+                    highlightRoute(0);
+
+                    //bounds = line.getBounds(bounds);
+                    google.maps.event.addListener(line, 'click', function () {
+                        // detect which route was clicked on
+                        var index = polylines.indexOf(this);
+                        highlightRoute(index);
+                    });
+                }
+                //map.fitBounds(bounds);
+            } else {
+                alert("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
+            }
+        });
+    }
+
+    function placeMarker(location) {
+
+        directionsRenderer.setDirections({routes: []});
+        for (var i in markers) {
+            markers[i].setMap(null);
+        }
+        markers.pop();
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            draggable: true
+        });
+        markers.push(marker);
+        calcRoute();
+    }
+
+    function displayPathElevation(path, elevator, map) {
+        // Display a polyline of the elevation path.
+        /*new google.maps.Polyline({
+         path: path,
+         strokeColor: '#ff0000',
+         strokeOpacity: 0.4,
+         map: map
+         });*/
+
+        // Create a PathElevationRequest object using this array.
+        // Ask for 256 samples along that path.
+        // Initiate the path request.
+        elevator.getElevationAlongPath({
+            'path': path,
+            'samples': 256
+        }, plotElevation);
+    }
+
+    function plotElevation(elevations, status) {
+
+        var chartDiv = document.getElementById('elevation_chart');
+        if (status !== 'OK') {
+            // Show the error code inside the chartDiv.
+            chartDiv.innerHTML = 'Cannot show elevation: request failed because ' +
+                status;
+            return;
+        }
+        // Create a new chart in the elevation_chart DIV.
+        var chart = new google.visualization.ColumnChart(chartDiv);
+
+        // Extract the data from which to populate the chart.
+        // Because the samples are equidistant, the 'Sample'
+        // column here does double duty as distance along the
+        // X axis.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Sample');
+        data.addColumn('number', 'Elevation');
+        for (var i = 0; i < elevations.length; i++) {
+            data.addRow(['', elevations[i].elevation]);
+        }
+
+        // Draw the chart using the data within its DIV.
+        chart.draw(data, {
+            height: 150,
+            legend: 'none',
+            titleY: 'Elevation (m)'
+        });
+
+        var elevationDetails = calculateAvgAndMax(elevations);
+        $('#divElevation').html('<div><b>Average Elevation: </b>' + elevationDetails.avg + ' meters <br/>' +
+            '<b>Maximum Elevation: </b>' + elevationDetails.max + ' meters' +
+            '</div>');
+    }
+
+    function drawCircle() {
+        if (circle != null) {
+            circle.setMap(null);
+        }
+        circle = new google.maps.Circle({
+            map: map,
+            radius: 1609.344 * $('#milesToTravel').val(),    // in meters
+            fillColor: '#ff56ab',
+            center: currentLocation
+        });
+
+        map.fitBounds(circle.getBounds());
+
+        google.maps.event.addListener(circle, 'click', function (ev) {
+            placeMarker(ev.latLng);
+        });
+        //circle.bindTo('center', currentLocation, 'position');
+    }
+
+    function highlightRoute(index) {
+        for (var j in  polylines) {
+            var color = (j == index) ? '#0000ff' : '#999999';
+            polylines[j].setOptions({strokeColor: color});
+
+            displayPathElevation(routes[index], elevator, map);
+        }
+    }
+
+    function drawPolyline(path, color) {
+        var line = new google.maps.Polyline({
+            path: path,
+            strokeColor: color,
+            strokeOpacity: 0.7,
+            strokeWeight: 10
+        });
+        line.setMap(map);
+        return line;
+    }
+
+    /**
+     * @returns {number} Average elevation in meters
+     */
+    function calculateAvgAndMax(elevations) {
+        var sum = 0;
+        var max = 0;
+        $.each(elevations, function (i, v) {
+            if (!isNaN(v.elevation)) {
+                sum += Number(v.elevation);
+
+                if (v.elevation > max) {
+                    max = v.elevation;
+                }
+            }
+        });
+        var len = elevations.length;
+        return {avg: (sum / len).toFixed(2), max: max.toFixed(2)};
+    }
+
+    function displayLocationElevation(location, elevator, infowindow) {
+        // Initiate the location request
+        elevator.getElevationForLocations({
+            'locations': [location]
+        }, function (results, status) {
+            infowindow.setPosition(location);
+            if (status === 'OK') {
+                // Retrieve the first result
+                if (results[0]) {
+                    // Open the infowindow indicating the elevation at the clicked position.
+                    infowindow.setContent('The elevation at this point <br>is ' +
+                        results[0].elevation + ' meters.');
+                } else {
+                    infowindow.setContent('No results found');
+                }
+            } else {
+                infowindow.setContent('Elevation service failed due to: ' + status);
+            }
+        });
+    }
+
+    $('#btnGo').click(function (e) {
+        findLocation();
+    });
+}
+
